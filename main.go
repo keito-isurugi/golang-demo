0124f8903e47fc135a2287c2bcaa15fadb9bd49e
KeitoIsurugi <isurugi@csplan.jp>
2023-01-20 08:28:00 +0900

    net/httpでルーティング

--- main.go
+++ main.go
@@ -1,34 +1,34 @@
-// package main
-
-// import (
-// 	// "encoding/json"
-// 	// "fmt"
-// 	// "log"
-// 	// "net/http"
-
-// 	_ "github.com/go-sql-driver/mysql"
-	// "golang_demo/router"
-// 	// "golang_demo/db"
-// )
-
-// func main() {
-// 	router.Router()
-// 	// db.Db()
-// }
-
-
 package main
 
 import (
-	"fmt"
-	"net/http"
+	// "encoding/json"
+	// "fmt"
+	// "log"
+	// "net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"golang_demo/router"
+	// "golang_demo/db"
 )
 
-func helloHander(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Normal isurugi</h1>")
+func main() {
+	router.Router()
+	// db.Db()
 }
 
-func main() {
-	http.HandleFunc("/", helloHander)
-	http.ListenAndServe(":8080", nil)
-}
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"net/http"
+// )
+
+// func helloHander(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "<h1>Hello Normal isurugi</h1>")
+// }
+
+// func main() {
+// 	http.HandleFunc("/", helloHander)
+// 	http.ListenAndServe(":8080", nil)
+// }
--- router/router.go
+++ router/router.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	// "os"
-
 	// _ "github.com/go-sql-driver/mysql"
 	// gorm "github.com/jinzhu/gorm"
 	// godotnev "github.com/joho/godotenv"
 )
+
+
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to tha HomaPage!")
@@ -37,9 +39,61 @@ func handleReauests() {
 // }
 
 func Router() {
-	fmt.Println("router!!")
-	// http.HandleFunc("/", homePage)
+	// r := NewRouter()
+	// http.ListenAndServe(":8080", r)
+	// fmt.Println("router!!")
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/users/", UserHandler)
 	// http.HandleFunc("/articles", returnAllArticles)
 	// http.HandleFunc("/players", fetchPlayers)
-	// log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// type Router struct{}
+
+// func NewRouter() http.Handler {
+// 	r := &Router{}
+// 	return r
+// }
+
+// func (router *Router) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+// endpoint, param := SeparatePath(...)
+
+// 	switch endpoint {
+// 	case "/users":
+// 		switch r.Method {
+// 		case http.MethodGet:
+// 			UserHandler(w, r, param)
+// 		default:
+// 			MethodNotAllowedHandler(w, r)
+// 		}
+// 	default:
+// 		NotFoundHandler(w, r)
+// 	}
+// }
+
+func UserHandler(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimPrefix(r.URL.Path, "/users/")
+	i := strings.Index(path, "/")
+	userId, path := path[:i], path[i:]
+	
+	fmt.Println("users userId:", userId)
+	// fmt.Println("users path:", path)
+	PostHndler(w, r, path, userId)
+}
+
+func PostHndler(w http.ResponseWriter, r *http.Request, path string, userId string) {
+	path = strings.TrimPrefix(path, "/posts/")
+	i := strings.Index(path, "/")
+
+	var postId string
+	if(i <= 0) {
+		postId = path
+	} else {
+		postId, path = path[:i], path[i:]
+	}
+
+	// CommentHandler(w, r, path, userId, postId)
+	// fmt.Println("posts path:", path)
+	fmt.Println("posts postId:", postId)
 }
