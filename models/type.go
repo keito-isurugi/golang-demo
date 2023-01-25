032d337c008280e94c27fd10ba6d69c126fd241f
keito-isurugi <ja224kg@gmail.com>
2023-01-25 15:13:30 +0900

    User, Todoの構造体にjsonのプロパティ名を追加

--- models/type.go
+++ models/type.go
@@ -5,21 +5,21 @@ import (
 )
 
 type User struct {
-	ID int `gorm:"primary_key"`
-  Name string
-	Email string
-	Password int
-	CreatedAt time.Time
-  UpdatedAt time.Time
-	Todos []Todo
+	ID int `gorm:"primary_key" json:"id"`
+  Name string `json:"name"`
+	Email string `json:"email"`
+	Password int `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+	Todos []Todo `json:"todos"`
 }
 
 type Todo struct {
-	ID int `gorm:"primary_key"`
-	UserID int
-  Title string
-  Content string
-	CreatedAt time.Time
-  UpdatedAt time.Time
-	User User
+	ID int `gorm:"primary_key" json:"id"`
+	UserID int `json:"user_id"`
+  Title string `json:"title"`
+  Content string `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+	User User `json:"user"`
 }
