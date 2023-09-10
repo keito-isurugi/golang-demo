4c05dfad03d4587a002ac1151543f6d6a58e6111
KeitoIsurugi <ja224kg@gmail.com>
2023-09-10 23:09:40 +0900

    commit

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"golang_demo/router"
+// 	"golang_demo/auth"
+// 	"golang_demo/demo"
+// )
+
+// func main() {
+// 	auth.Auth()
+// 	router.RouterPokemon()
+// 	router.Router()
+// 	demo.Hoge()
+// }
+
 package main
 
-import (
-	// "fmt"
-	"golang_demo/router"
-	// "golang_demo/auth"
-	// "golang_demo/demo"
-)
+import "fmt"
+
+func zeroval(ival int) {
+    ival = 0
+    // fmt.Println("zeroval:", ival)
+
+}
+
+func zeroptr(iptr *int) {
+    *iptr = 0
+}
 
 func main() {
-	// auth.Auth()
-	// router.RouterPokemon()
-	router.Router()
-	// demo.Hoge()
+    i := 1
+    // fmt.Println("initial:", i)
+
+    // zeroval(i)
+    // fmt.Println("zeroval:", i)
+
+    zeroptr(&i)
+    fmt.Println("zeroptr:", i)
+
+    fmt.Println("pointer:", &i)
+
+		fmt.Println(11233)
 }
